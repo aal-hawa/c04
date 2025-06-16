df0d379592f2437c9587e04b1e6118e39ef37c78
aal-hawa <aal-hawa@student.42abudhabi.ae>
2025-06-16 18:03:47 +0400

    first commit

---
+++ ex01/main.cpp
@@ -0,0 +1,28 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+
+int main()
+{
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak
+	delete i;
+
+	std::cout << "\n--------- Animal Array Test ---------" << std::endl;
+
+	Animal* animals[10];
+	
+	for(int j = 0; j < 5; j++)
+		animals[j] = new Dog();
+	for(int j = 5; j < 10; j++)
+		animals[j] = new Cat();
+
+	for(int j = 0; j < 10; j++)
+		delete animals[j];
+		
+	return 0;
+}
